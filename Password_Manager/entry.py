d34a8f2ade652c64eb3a0d9a5ccb12b23b1005db
00mk1 <110798338+00mk1@users.noreply.github.com>
2022-08-18 10:42:01 +1200

    Add files via upload

---
+++ Password_Manager/entry.py
@@ -0,0 +1,118 @@
+import generate_password
+import pyAesCrypt
+import os
+import pyperclip
+import platform
+
+def add_entry(key):
+    system = platform.system()
+    cwd = os.getcwd()
+    title = str(input("Enter A Title:\n"))
+    username = input("Enter The Username, Email Address or Leave Blank:\n")
+    if system == 'Windows':
+        file_path = cwd + "\Password_Manager\Saved_Passwords" + title + ".txt"
+    else:
+        file_path = cwd + "\Password_Manager\Saved_Passwords" + title + ".txt"
+
+    mode = input("[1] Generate Random Password \n[2] Add Password Manually\n[b] Back\n")
+    if mode == '1':
+        password = generate_password.generate_password()
+
+    if mode == 'b':
+        add_entry(key)
+
+    else:
+        password = input("Enter Password:\n")
+
+
+    try:
+        file = open(file_path, 'x')
+    except:
+        if input("This File Already Exists, Would You Like To Overwrite It? [y][n]\n") == 'y':
+            file = open(file_path, 'w')
+
+    file_text = "Title: " + title + '\n' + "Username: " + username + '\n' + "Password: " + password
+    file.write(file_text)
+    file.close()
+    output = file_path + ".aes"
+    try:
+        pyAesCrypt.encryptFile(file_path, output, key)
+        os.remove(file_path)
+        if input("Copy Password To Clipboard? [y][n]\n") == 'y':
+            pyperclip.copy(password)
+        return 
+
+    except:
+        print("The encyption key is incorrect or the file is corrupt.")
+        return
+
+    
+def read_entry(key, mode):
+    x = 1
+    res = []
+    if os.system == 'Windows':
+        dir_path = os.getcwd() + '\Password_Manager\Saved_Passwords'
+    else:
+        dir_path = os.getcwd() + '/Password_Manager/Saved_Passwords/'
+
+    for path in os.listdir(dir_path):
+        
+        # check if current path is a file
+        if os.path.isfile(os.path.join(dir_path, path)):
+            res.append(path)
+    
+    #checks if there are files to list, read or delete
+    if len(res) == 0:
+        print("There are no files")
+        return
+
+    else:
+        print("Saved Passwords:\n")
+        for results in res:
+            print(str(x) + ". " + results) 
+            x += 1
+
+        if mode == 'read':
+            file_path = dir_path + res[int(input("Select File To Read:\n"))-1]
+            output = str(file_path).replace('.aes', '')
+
+            print('\n\n\n' + str(file_path))
+            pyAesCrypt.decryptFile(str(file_path), output, key)
+            sel = open(output)
+            print(sel.read() + '\n\n\n')
+            sel = open(output)
+            os.remove(output)
+            if input("Copy Password To Clipboard? [y][n]\n") == 'y':
+                pyperclip.copy(sel.readlines()[2].replace('Password:', ''))
+                
+            
+            
+        
+        if mode == 'delete':
+
+            try:
+                file_path = dir_path + res[int(input("Select File To Delete:\n"))-1]
+                if input("Confirm [y][n]\n") == 'y':
+                    os.remove(file_path)
+                else:
+                    read_entry(key, 'delete')
+            except:
+                return
+
+
+
+    
+           
+
+    
+
+    
+
+    
+
+    
+
+
+
+
+
