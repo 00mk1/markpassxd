b124d767cd40301378ee187a238160176c40ed7a
00mk1 <110798338+00mk1@users.noreply.github.com>
2022-09-08 12:30:00 +1200

    Add files via upload

---
+++ MarkPassxD_v1.0.3_Linux/README.md
@@ -0,0 +1,30 @@
+# MarkPassxD - A Simple CLI Password Manager
+FOSS - Mark Barlass 2022, use as you wish.
+
+
+## Installation Instructions
+
+#### Dependancies 
+This program requires the following dependancies and will not work unless they are installed:
+
+pyAesCrypt
+Pyperclip
+Hashlib
+
+These can be installed with the following commands:
+
+pip install pyperclip
+pip install pyAesCrypt
+pip install hashlib
+
+#### Method One (GitHub)
+Go to 'https://github.com/00mk1/markpassxd/releases' in your browser, find the most recent release and download the correct version for your device from the 'assets' section. Extract the downloaded .zip file to the desired location e.g. 'Home/ or Users\You\Program Files\'.
+
+#### Method Two (source code)
+If you have git installed on your device, open a terminal and enter the following:
+'git clone https://github.com/00mk1/markpassxd.git'.
+
+
+## Use Instructions
+Run 'main.py'
+
---
+++ MarkPassxD_v1.0.3_Linux/login.py
@@ -0,0 +1,98 @@
+#MarkPassxD - Mark Barlass 2022
+
+#Handles creating a master password, logging in and changing the master password.
+
+#mode is the operating mode of this function. It can be used in normal mode or reset [master password] mode.
+def login(mode):
+    import platform
+    import hashlib 
+    import os
+    import pyAesCrypt
+  
+   
+    mp_file_path = os.getcwd() + '/Saved_Passwords/master_password/master_password.txt'
+    salt_path = os.getcwd() + '/Saved_Passwords/master_password/salt.txt'
+
+    #reads the master password.
+    master_password_file = open(mp_file_path)
+    master_password = master_password_file.read()
+    
+
+    #if the master password is already set.
+    if master_password != '':
+        #the salt is a string added to the password to make it more secure.
+        salt_file = open(salt_path)
+        salt = salt_file.read()
+        
+        #gets the user input and adds the salt value.
+        usr_imput = input("Enter Master Password:\n") + salt
+
+        #compares the hased user input with the saved password.
+        if hashlib.sha256(usr_imput.encode('utf-8')).hexdigest() == master_password:  
+            print("Success!")
+        else:
+            print("Incorrect Password\n")
+            login('normal')
+
+        master_password_file.close()
+
+    #creates a new master password if none is set
+    else:
+        salt_file = open(salt_path, 'w')
+        #writes a random salt to the salt file
+        salt_file.write(str(os.urandom(32)))
+        salt_file = open(salt_path, 'r')
+        salt = salt_file.read()
+
+        master_password = open(mp_file_path, 'w')
+        usr_imput = input("Enter New Master Password:\n") + salt
+        #writes the new master password
+        master_password.write(hashlib.sha256(usr_imput.encode('utf-8')).hexdigest())
+    
+    salt_file.close()
+    
+    #generates a key to use for file encyption
+    encrypt_hash = hashlib.sha224(usr_imput.encode('utf-8')).hexdigest()
+
+    if mode == 'normal':
+        return encrypt_hash
+
+    #allows the user to change the master password.
+    if mode == 'reset':
+        new_password = str(input("Enter New Master Password:\n"))
+
+        if input("Confirm [y][n]\n") == 'y':
+            #derives a new encypt/decrypt key from new master password
+            key = str(hashlib.sha224(new_password.encode('utf-8')).hexdigest())
+    
+            res = []
+
+            dir_path = str(os.getcwd()) + "/Saved_Passwords/"
+            
+            #for each saved password
+            for path in os.listdir(dir_path):
+                # check if current path is a file
+                if os.path.isfile(os.path.join(dir_path, path)):
+
+                    res.append(path)
+                    file_path = dir_path + path
+                    output = file_path.replace('.aes', '')
+
+                    #decrypts saved password with old password
+                    pyAesCrypt.decryptFile(file_path, output, encrypt_hash)
+                    os.remove(file_path)
+                    #re-encrypts file wth new password
+                    pyAesCrypt.encryptFile(output, file_path, key)
+                    os.remove(output)
+                    #saves the new master password
+                    master_password = open(mp_file_path, 'w')
+                    master_password.write(str(hashlib.sha256(new_password.encode('utf-8')).hexdigest()))
+                    master_password_file.close()
+                    return key
+
+        else:
+            return master_password
+   
+
+
+
